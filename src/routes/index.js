e1ad6dc6d23cba10c52ea433145232e81b2ab645
Muhammad Hassan <hassanshakil12@gmail.com>
2025-06-28 03:58:46 +0500

    Fix: Route Index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const app = express();
 const authRoutes = require("./auth.route");
 const adminRoutes = require("./admin.route");
 const employeeRoutes = require("./employee.route");
+const commonRoutes = require("./common.route");
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/employee", employeeRoutes);
+app.use("/api/v1/common", commonRoutes);
 
 module.exports = app;

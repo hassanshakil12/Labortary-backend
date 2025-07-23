6b1030182c04d8b4dc198ddfce69087afc139f73
Muhammad Hassan <hassanshakil12@gmail.com>
2025-07-23 05:51:05 +0500

    Fix: Rate Limiter Removed

--- server.js
+++ server.js
@@ -66,19 +66,19 @@ app.use(
   })
 );
 
-const apiRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: {
-    success: false,
-    message:
-      "Too many requests from this IP, please try again after 15 minutes",
-  },
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+// const apiRateLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   message: {
+//     success: false,
+//     message:
+//       "Too many requests from this IP, please try again after 15 minutes",
+//   },
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
+// app.use(apiRateLimiter);
 
-app.use(apiRateLimiter);
 app.use(require("./src/routes/index"));
 
 const adminSeeder = async () => {
